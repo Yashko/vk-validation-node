66656cae6fc1f4e9bf25f4f7fef01e3eefadb3b5
Alexandr <yashko2010@yandex.ru>
2016-10-19 19:36:17 +0300

    phone as second argument
    now you can pass phone code as second argument when launching (node validation.js a1b2c3 54321)

--- validation.js
+++ validation.js
@@ -7,6 +7,7 @@ const rl = readline.createInterface({
 });
 let inpHash = process.argv[2];
 if (!inpHash) throw new Error("Please specify hash as first argument");
+let inpPhone = process.argv[3] || false;
 let defaultUrl = function(hash) {
 	return `https://m.vk.com/login?act=security_check&api_hash=${hash}`
 };
@@ -14,16 +15,20 @@ let confirmUrl = function(hash,api_hash) {
 	return `https://m.vk.com/login.php?act=security_check&to=&hash=${hash}&api_hash=${api_hash}`
 };
 let regexp = /act=security_check&to=&hash=(.*)&api_hash=(.*)"/g;
+let confirm = function(code) {
+	request.post(confirmUrl(match[1], match[2]), {form: {code: code} }, function(error, response, body) {
+		if (error) return console.log(error);
+		console.log("Validation finished.");
+		process.exit();
+	});
+}
 request(defaultUrl(inpHash), function(error, response, body) {
 	if (error) return console.log(error);
 	var match = regexp.exec(body);
 	if (!match) throw new Error("Cant find hashes, probably session expired");
+	if (inpPhone) return confirm(inpPhone);
 	rl.question('Input phone code:\n', function(code) {
-		request.post(confirmUrl(match[1], match[2]), {form: {code: code} }, function(error, response, body) {
-			if (error) return console.log(error);
-			console.log("Validation finished.");
-			rl.close();
-			process.exit();
-		});
+		confirm(code);
+		rl.close();
 	});
 });
